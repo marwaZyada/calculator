03535eeefeb782f9595b0e477a060129c47893a9
marwazyada <zyadamarwa6@gmail.com>
2022-06-04 13:31:12 +0200

    calc

---
+++ script.js
@@ -0,0 +1,24 @@
+let btn = document.querySelectorAll(".operation");
+let operation = document.getElementById('in-val');
+let result = document.getElementById("result");
+let del = document.getElementById("del");
+let clr = document.getElementById("clr");
+
+
+btn.forEach((el) => {
+    el.addEventListener('click', (att) => {
+        operation.value += att.target.value;
+
+    })
+})
+
+
+let calculate = () => eval(operation.value);
+result.addEventListener("click", () => {
+    if (operation.value !== " ") {
+
+        try { operation.value = calculate() } catch (error) { operation.value = "error" }
+    } else { operation.value = "please enter numbers"; }
+});
+clr.addEventListener("click", () => operation.value = " ")
+del.addEventListener("click", () => operation.value = operation.value.slice(0, -1));
